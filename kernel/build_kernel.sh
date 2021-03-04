6d6b5b5add9a9e267da5c98b9fa683ec0754d905
ChoiHyeonSeong <chs0517@hybus.net>
2021-03-04 04:58:56 +0000

    Toolchain 9.2로 변경

--- kernel/build_kernel.sh
+++ kernel/build_kernel.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 
-make imx_v7_defconfig
+export ARCH=arm
+export CROSS_COMPILE='../Toolchain/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-'
+
+make imx_JW_defconfig
 make zImage -j9
 make dtbs
 
