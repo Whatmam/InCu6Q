2d28d76259a8d2b9483d3985a32a896173400545
ChoiHyeonSeong <chs0517@hybus.net>
2021-04-12 09:10:06 +0000

    buildroot 컴파일러 및 config 변경

--- Filesystem/build_Filesystem.sh
+++ Filesystem/build_Filesystem.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 export ARCH=arm
-export CROSS_COMPILE=$PWD/../Toolchain/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf
+export CROSS_COMPILE=$PWD/../Toolchain/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf
+if [ ! -d buildroot-2020.02.10 ]; then
+         echo "Not Find buildroot Directory ... !"
+         echo "Extract buildroot...."
+	tar zxvf buildroot-2020.02.10.tar.gz
+fi
+cp -vrfp buildroot_configs/imx6_incu6_defconfig buildroot-2020.02.10/configs/
+echo "Compile Start!!"
+cd buildroot-2020.02.10
 
 make clean
 make imx6_incu6_defconfig
