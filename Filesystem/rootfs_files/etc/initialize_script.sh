6c7f0c7655c1e3dbc3344c96a4ca6bc1f0fec21b
ChoiHyeonSeong <chs0517@hybus.net>
2021-05-03 03:01:22 +0000

    7인치 터치 Calibration 적용 및 파일명 변경

--- Filesystem/rootfs_files/etc/initialize_script.sh
+++ Filesystem/rootfs_files/etc/initialize_script.sh
@@ -3,7 +3,7 @@
 lcd_cmd=`cat /proc/cmdline | grep lcd`
 #echo $lcd_cmd
 if [ -z "$lcd_cmd" ]; then
-        cp -rfp /etc/10inch_pointcal /etc/pointcal
+	cp -rfp /etc/10inch_pointcal /etc/pointercal
 else
-        cp -rfp /etc/8inch_pointcal /etc/pointcal
+	cp -rfp /etc/7inch_pointcal /etc/pointercal
 fi
